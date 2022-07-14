b15ebdb7fc18c8e2b28c97bea875fb36c2dd8740
unknown <Pushkin@example.com>
2022-07-14 15:03:47 +0300

    Task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+
