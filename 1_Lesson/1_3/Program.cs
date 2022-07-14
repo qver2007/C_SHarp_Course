c6614d76b3a7b7aeb04d116636cdfc1c2ffd559b
unknown <Pushkin@example.com>
2022-07-14 15:59:42 +0300

    Task 1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+Console.WriteLine("Пн");
+}
+else if (a == 2)
+{
+Console.WriteLine("Вт");
+}
+else if (a == 3)
+{
+Console.WriteLine("Ср");
+}
+else if (a == 4)
+{
+Console.WriteLine("Чт");
+}
+else if (a == 5)
+{
+Console.WriteLine("Пт");
+}
+else if (a == 6)
+{
+Console.WriteLine("Сб");
+}
+else if (a == 7)
+{
+Console.WriteLine("Вс");
+}
+else
+{Console.WriteLine("Серьёзно?");}
