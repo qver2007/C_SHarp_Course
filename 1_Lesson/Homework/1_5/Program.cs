1204f13bab283f53de3c7441fd5f13e0ec5f06b2
unknown <Pushkin@example.com>
2022-07-15 07:05:29 +0300

    ДЗ1_5

---
+++ 1_Lesson/Homework/1_5/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Write number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Last digit is ");
+Console.WriteLine(a%10);
