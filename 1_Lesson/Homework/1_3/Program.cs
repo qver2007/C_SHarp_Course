f4a683c6a9bc2e5065fcd8650e1c2cbafd6ebf20
qver2007 <108473940+qver2007@users.noreply.github.com>
2022-07-15 07:46:07 +0300

    Delete 1_Lesson/Homework/1_3 directory
    случайно сделал 2 раза

--- 1_Lesson/Homework/1_3/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-Console.WriteLine("Write number: ");
-int a = int.Parse(Console.ReadLine());
-if (a%2==0)
-    Console.WriteLine("Число четное");
-else
-    Console.WriteLine("Число нечетное");
